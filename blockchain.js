8a1d631093d0a423cb5c35b0b25878b812d027eb
Herbrant <cdavide98carnemolla@gmail.com>
2022-04-13 18:14:45 +0200

    feat: prevent difficulty jumps

--- blockchain.js
+++ blockchain.js
@@ -19,18 +19,22 @@ class Blockchain {
 		if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
 			return false;
 
-			for (let i = 1; i < chain.length; i++) {
-				const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
-				const actualLastHash = chain[i-1].hash;
+		for (let i = 1; i < chain.length; i++) {
+			const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
+			const actualLastHash = chain[i-1].hash;
+			const lastDifficulty = chain[i-1].difficulty;
 
-				if (lastHash !== actualLastHash)
-					return false;
-				
-				const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+			if (lastHash !== actualLastHash)
+				return false;
+			
+			const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
-				if (hash !== validatedHash)
-					return false;
-			}
+			if (hash !== validatedHash)
+				return false;
+			
+			if (Math.abs(lastDifficulty - difficulty) > 1)
+				return false;
+		}
 
 		return true;
 	}
