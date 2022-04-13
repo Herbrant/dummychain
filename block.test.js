2858ba795decb054f88467445d44b69e280a47ee
Herbrant <cdavide98carnemolla@gmail.com>
2022-04-13 13:25:32 +0200

    feat: added mineBlock

--- block.test.js
+++ block.test.js
@@ -27,4 +27,26 @@ describe('Block', () => {
 			expect(genesisBlock).toEqual(GENESIS_DATA);
 		});
 	});
+
+	describe('mineBlock()', () => {
+		const lastBlock = Block.genesis();
+		const data = 'mined data';
+		const mineBlock = Block.mineBlock({ lastBlock, data });
+
+		it('return a Block instance', () => {
+			expect(mineBlock instanceof Block).toBe(true);
+		});
+
+		it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+			expect(mineBlock.lastHash).toEqual(lastBlock.hash);
+		});
+
+		it('sets the `data`', () => {
+			expect(mineBlock.data).toEqual(data);
+		});
+
+		it('sets a `timestamp`', () => {
+			expect(mineBlock.timestamp).not.toEqual(undefined);
+		});
+	});
 });
